da586bed69c594c891dfebe10ce5cd68bf9e7f60
osidekyle <hawkinskyle67@gmail.com>
2022-06-01 23:20:05 -0700

    svm example

---
+++ svm.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn import preprocessing, model_selection, neighbors, svm
+import pandas as pd
+
+df = pd.read_csv("breast-cancer-wisconsin.data.txt")
+
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], axis=1, inplace=True)
+
+X = np.array(df.drop(['class'], axis=1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVC()
+
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+
